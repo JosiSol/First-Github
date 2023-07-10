631f045e74bdb96f245b264d0b14086196a45cf0
Yosef Solomon <121563505+JosiSol@users.noreply.github.com>
2023-07-10 22:12:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Yosef Solomon on 7/9/23.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int x;
+    cout<< "Enter the number"<<endl;
+    cin>>x;
+    if (x % 2 == 0 )
+        cout<< "It is even. "<<endl;
+    else
+        cout<< "It is odd "<<endl;
+    return 0;
+}
+
+
