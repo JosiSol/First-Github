ac812ca737044910b0422e4ede1f0ad469774247
Yosef Solomon <121563505+JosiSol@users.noreply.github.com>
2023-07-10 22:09:33 +0300

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,sum;
+    cout<<"Enter the first number\n";
+    cin>>x;
+    cout<<"Enter the second number\n";
+    cin>>y;
+    sum = x + y;
+    cout<<"The sum of " << x << " and " << y <<" is "<<sum;
+    return 0;
+}
+
+
+
+
